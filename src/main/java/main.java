6929776d09e1fef5950003426abde7ce73879e42
tuyen.luong <atuyenluong@gmail.com>
2024-12-20 18:23:14 +0700

    Deep dive on singleton bean scope

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,18 +2,19 @@ import beans.User;
 import beans.Vehicle;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.VehicleServices;
 
 public class main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        User user = context.getBean(User.class);
-        Vehicle vehicle = context.getBean(Vehicle.class);
-        System.out.println("User name is: "+ user.getName());
-        System.out.println("Vehicle name is: "+ user.getVehicle().getName());
-        System.out.println("User vehicle is: "+ user.getVehicle());
-        System.out.println("Vehicle speaker is: "+ user.getVehicle().getVehicleServices().getSpeaker());
-        user.getVehicle().getVehicleServices().getSpeaker().makeSound();
-        System.out.println("Vehicle tyres is: "+ user.getVehicle().getVehicleServices().getTyres());
-        user.getVehicle().getVehicleServices().getTyres().moveCar();
+        VehicleServices vehicleServices1 = context.getBean(VehicleServices.class);
+        VehicleServices vehicleServices2 = context.getBean("vehicleServices",VehicleServices.class);
+        System.out.println("Hashcode of the vehicleServices1 object: " + vehicleServices1.hashCode());
+        System.out.println("Hashcode of the vehicleServices2 object: " + vehicleServices2.hashCode());
+        if(vehicleServices1==vehicleServices2){
+            System.out.println("VehiclesService bean is a singleton scoped bean");
+        }else{
+            System.out.println("VehiclesService bean is not singleton scoped bean");
+        }
     }
 }
--- src/main/java/services/VehicleServices.java
+++ src/main/java/services/VehicleServices.java
@@ -3,9 +3,13 @@ package services;
 import implementation.MichelinTyres;
 import implementation.SonySpeaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_SINGLETON) // SCOPE_SINGLETON is the default scope of a bean in Spring
+                                       // So if you don't declare the @Scope for the @Component it still created as SCOPE_SINGLETON
 public class VehicleServices {
 
     private SonySpeaker speaker;
