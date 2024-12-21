5f3e50104921189d05aefef81c727a7a49612a76
tuyen.luong <atuyenluong@gmail.com>
2024-12-21 17:06:36 +0700

    The differences between Singleton Spring and Singleton pattern

--- src/main/java/services/VehicleServices.java
+++ src/main/java/services/VehicleServices.java
@@ -10,6 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(BeanDefinition.SCOPE_SINGLETON) // SCOPE_SINGLETON is the default scope of a bean in Spring
                                        // So if you don't declare the @Scope for the @Component it still created as SCOPE_SINGLETON
+
+// Singleton in Spring is different from Singleton pattern.
+// They still do the job of make sure only one object type is existing, but they live in different context.
+// - Singleton in Spring live in Spring Container context.
+// - Singleton pattern live in JVM context
 public class VehicleServices {
 
     private SonySpeaker speaker;
