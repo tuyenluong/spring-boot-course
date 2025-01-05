bd9f2eadca891cad7c9e02030417b80404f67fc6
tuyenluong <76527250+tuyenluong@users.noreply.github.com>
2025-01-06 01:15:56 +0700

    Understand Thymeleaf caching

--- src/main/java/com/jeremy/controller/HomeController.java
+++ src/main/java/com/jeremy/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.jeremy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "home"})
-    public String displayHomePage(){
+    public String displayHomePage(Model model){
+        model.addAttribute("userName", "Mr.Smith");
         return "index.html";
     }
 }
