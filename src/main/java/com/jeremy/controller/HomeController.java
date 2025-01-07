f4aa4223b41a2269284c448596df7ca72abc2d96
tuyenluong <76527250+tuyenluong@users.noreply.github.com>
2025-01-08 00:10:54 +0700

    Using FE code build a School Management system

--- src/main/java/com/jeremy/controller/HomeController.java
+++ src/main/java/com/jeremy/controller/HomeController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "home"})
-    public String displayHomePage(Model model){
-        model.addAttribute("userName", "Mr.Smith");
+    public String displayHomePage(){
         return "index.html";
     }
 }
