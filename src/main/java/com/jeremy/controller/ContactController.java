9343271a502199feb14890907ff5cba567c6611e
tuyenluong <76527250+tuyenluong@users.noreply.github.com>
2025-01-10 23:45:23 +0700

    Build a submit form function

--- src/main/java/com/jeremy/controller/ContactController.java
+++ src/main/java/com/jeremy/controller/ContactController.java
@@ -1,10 +1,16 @@
 package com.jeremy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
+    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
 
     // Because we might have use case for the user to fill in contact form
     // Therefore it's necessary to add more functionality
@@ -15,4 +21,14 @@ public class ContactController {
         return "contact.html";
     }
 
+    @RequestMapping(value = "/saveMsg", method = RequestMethod.POST)
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message){
+        log.info("Name: "+name);
+        log.info("Mobile Number: "+mobileNum);
+        log.info("Email Address: "+email);
+        log.info("Subject: "+subject);
+        log.info("Message: "+message);
+        return new ModelAndView("redirect:/contact");
+    }
 }
