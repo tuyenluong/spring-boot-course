a9f449c3e8a034ffb251f02b4d512c3b4feff5b5
Jeremy Luong <76527250+tuyenluong@users.noreply.github.com>
2025-01-10 17:40:58 +0700

    Using addViewControllers of WebMvcConfigurer implementation

---
+++ src/main/java/com/jeremy/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.jeremy.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // Using WebMvcConfingurer to handle dynamic page
+    // which will not handle any business logic
+    // other than parsing the html page and Thymeleaf syntax
+    // then you can use this method addViewControllers of WebMvcConfigurer
+    // to handle this scenario
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/contact").setViewName("contact");
+    }
+
+}
