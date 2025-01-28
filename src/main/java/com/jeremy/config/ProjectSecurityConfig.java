d94d7ae5ae1c1cb4f0516c42b7b1ce27dfd4b31b
tuyen.luong <atuyenluong@gmail.com>
2025-01-28 17:57:45 +0700

    Demo disable CSRF

--- src/main/java/com/jeremy/config/ProjectSecurityConfig.java
+++ src/main/java/com/jeremy/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,10 +12,23 @@ public class ProjectSecurityConfig {
 
     @Bean
     public SecurityFilterChain projectSecurity(HttpSecurity http) throws Exception{
-        // Permit all requests inside the web application
-        http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+        // Custom security for each request
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("/", "/home").authenticated()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+
+        // Permit all requests inside the web application
+//        http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
 
         // Deny all requests inside the web application
 //        http.authorizeHttpRequests(request -> request.anyRequest().denyAll())
